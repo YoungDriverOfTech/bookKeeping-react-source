af9d354788739cab4209462a04b2e5a7eb4ec127
YoungDriverOfTech <xiaoleliang045@gmail.com>
2020-06-10 18:51:56 +0900

    编辑 tag 名

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
-import React, {ChangeEventHandler, useRef} from 'react';
+import React, {ChangeEventHandler} from 'react';
 import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
-  padding: 0 16px;
+  padding: 14px 16px;
   font-size: 14px;
 `;
 
 type Props = {
   value: string,
-  onChange: (value:string) => void
+  onChange: (value: string) => void
 }
 const NoteSection: React.FC<Props> = (props) => {
   const note = props.value;
@@ -20,7 +20,8 @@ const NoteSection: React.FC<Props> = (props) => {
 
   return (
     <Wrapper>
-      <Input label="备注" type="text" value={note} onChange={onChange}/>
+      <Input label="备注" type="text" value={note} onChange={onChange}
+             placeholder="请填写备注"/>
     </Wrapper>
   );
 };
