6914853f58ee12ae582c33c3a29c98a1e9ff290c
YoungDriverOfTech <xiaoleliang045@gmail.com>
2020-06-11 15:05:58 +0900

    展示所有记录

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -20,6 +20,11 @@ const defaultFormData = {
   amount: 0
 };
 
+const CategoryWrapper = styled.div`
+    background:#c4c4c4;
+`;
+
+
 function Money() {
   const [selected, setSelected] = useState(defaultFormData);
   const {records, addRecord} = useRecords();
@@ -38,21 +43,16 @@ function Money() {
     }
   };
 
-  useEffect(() => {
-    setTimeout(() => {
-      console.log('时间到');
-      setSelected({...selected, amount: 1000});
-    }, 3000);
-  }, []);
-
   return (
     <MyLayout>
       <TagsSection value={selected.tagIds}
                    onChange={tagIds => onChange({tagIds})}/>
       <NoteSection value={selected.note}
                    onChange={note => onChange({note})}/>
-      <CategorySection value={selected.category}
-                       onChange={category => onChange({category})}/>
+      <CategoryWrapper>
+        <CategorySection value={selected.category}
+                         onChange={category => onChange({category})}/>
+      </CategoryWrapper>
       <NumberPadSection value={selected.amount}
                         onChange={amount => onChange({amount})}
                         onOk={submit}
